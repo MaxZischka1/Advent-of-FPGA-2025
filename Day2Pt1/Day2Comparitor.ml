3340469646c16187992095067c144f52eb9647a5
Max <max.zischka1@gmail.com>
2026-01-02 22:16:56 -0500

    Parser change

---
+++ Day2Pt1/Day2Comparitor.ml
@@ -0,0 +1,5 @@
+(*First we should subtract the upper and lower bound to provide 1 piece of data. The 
+number of digits changed. So now if we have a length input, data input and # of digits 
+  we want to be able to think of some way to concatenate with. 
+
+For the 1 digit check concatenate all of the first digit. For the second digit*)
